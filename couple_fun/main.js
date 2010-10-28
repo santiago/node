d1892d6564f29359db62b5a3ed9b2e76e15b1e94
santiago <sgaviria@gmail.com>
2010-10-28 09:02:30 -0500

    review

--- couple_fun/main.js
+++ couple_fun/main.js
@@ -23,22 +23,31 @@ app.post('/admin/couple_fun/games', function(req, res) {
     var game_data= req.param('game');
 
     var game = new Game();
-    game.title = game_data.name;
+    game.title = game_data.title;
+    game.synopsis= game_data.synopsis;
     game.author= game_data.author;
     game.rules= game_data.rules;
     game.warnings= game_data.warning;
     game.tips= game_data.tips;
-    game.locations= [{game_location_id: game_data.situation.id, description: game_data.situation.description.trim().toLowerCase()}];
     game.category= [{category_id: 1, name: game_data.category.trim().toLowerCase()}];
     game.release_date= new Date();
+    game.locations= (function(situations) {
+	var array=[];
+	for (var i in situations) {
+	    array.push({game_location_id: situations[i].id, description: situations[i].description.trim().toLowerCase()});
+	}
+	return array;
+    })(game_data.situations);
+    
     game.save(function(a){});
-
-    GameLocation.findById(game_data.situation.id, function(situation) {
-	situation.games.push({game_id:game._id, title:game_data.name});
-	situation.save(function(a){});
-    });
-
-
+    
+    for (var sit in game.locations) {
+	GameLocation.findById(sit.id, function(situation) {
+	    situation.games.push({game_id:game._id, title:game_data.name});
+	    situation.save(function(a){});
+	});
+    }
+    
     res.send([]);
 });
 
@@ -52,6 +61,8 @@ app.get('/couple_fun/games', function(req, res) {
     var where= {};
     if (req.query.situation) {
 	where= {'locations.description':req.query.situation};
+    } else if (req.query.situation_id) {
+	where= {'locations._id':req.query.situation_id};
     }
     Game.find(where).all(function(array) {
 	res.send(clean_for_json(array));
